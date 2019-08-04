5e2be94f97acebfb4b2cd72b3f14350127f199cc
sahan hasitha <info@speralabs.com>
2019-08-04 20:50:36 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home');
 
 Route::get('login/{provider}', 'Auth\SocialController@redirect');
 Route::get('login/{provider}/callback','Auth\SocialController@callback');
