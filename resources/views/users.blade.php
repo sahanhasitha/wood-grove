04e7e778151f2b4fb05fed030796501386416193
sahan hasitha <info@speralabs.com>
2019-08-13 22:57:19 +0530

    add users

---
+++ resources/views/users.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <section role="main" class="content-body">
+        <header class="page-header">
+            <h2>Registrations</h2>
+        </header>
+
+        <!-- start: page -->
+        <div class="row">
+            <div class="col-lg-6 mb-3">
+                <section class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">System ID</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">EMail</th>
+                                    <th scope="col">Registered Through</th>
+                                    <th scope="col">Social Media ID</th>
+                                    <th scope="col">Registered On</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                @foreach (\App\User::all() as $user)
+
+                                    <tr>
+                                        <td>{{ $user->id }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->provider }}</td>
+                                        <td>{{ $user->provider_id }}</td>
+                                        <td>{{ $user->created_at }}</td>
+                                    </tr>
+
+                                @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <!-- end: page -->
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('home', 'HomeController@index')->name('home');
 Route::get('login/{provider}', 'Auth\SocialController@redirect');
 Route::get('login/{provider}/callback','Auth\SocialController@callback');
 Route::post('social-register','Auth\SocialController@doRegister')->name('social-register');
+
+Route::view('registrations', 'users');
